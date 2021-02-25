7f6bbe5b7d4d6d011ed0077cac0927a2d803ca12
TuanAnh0108 <57340581+TuanAnh0108@users.noreply.github.com>
2021-02-25 11:22:23 +0900

    Update ZoneCalculator.py

--- ZoneCalculator.py
+++ ZoneCalculator.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
+
 from fractions import Fraction
+import Credentials
 
 # Manual before running the code
 # Step 1: change the service_account_file to company one
@@ -9,38 +9,60 @@ from fractions import Fraction
 # Step 3: Change the name of the sheet to sheet1 for all of three spreadsheets
 
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = 'keys.json'
-
-creds = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-# If modifying these scopes, delete the file token.pickle.
-
-# Step 1: The ID of the spreadsheets
-ingredientSheet_ID = '1VkSVE1wUSWzIL6tL7iz9D4fsSvkgSY7svvwkmLb9_e0'
-inputListSheet_ID = '1ecMsdBj8GK1HL2ps-ouV0Zu3fqLD-BYmCylM5KZb_MA'
-outputListSheet_ID = '1fV96CIiY57L4Xje0Kr7BLIwt0Gjoyuq1B4-mpIamkVk'
-
-service = build('sheets', 'v4', credentials=creds)
-
-# Call the Sheets API
-sheet = service.spreadsheets()
-
 # Step 2: Access the spreadsheet tab and get the values inside each spreadsheet
 
-input_recipe = sheet.values().get(spreadsheetId=inputListSheet_ID,
-                                  range="sheet1!A1:C6").execute()
+
+# Get the name of the input and output sheet
+input_recipe_sheet = input("Enter the input name worksheet: ")
+output_recipe_sheet = input("Enter the output name worksheet: ")
+
+rangeInputS = input_recipe_sheet + "!A1:C5"
+rangeOutputS = output_recipe_sheet + "!A1"
+
+
+# Create output sheet with input recipe name
+
+# Add new worksheet into spreadSheet with ID: outputListID and title: sheet_name
+def add_sheets(outputListID, sheet_name):
+    try:
+        request_body = {
+            'requests': [{
+                'addSheet': {
+                    'properties': {
+                        'title': sheet_name,
+                        'tabColor': {
+                            'red': 0.44,
+                            'green': 0.99,
+                            'blue': 0.50
+                        }
+                    }
+                }
+            }]
+        }
+
+        response = Credentials.sheet.batchUpdate(
+            spreadsheetId=outputListID,
+            body=request_body
+        ).execute()
+
+        return response
+    except Exception as e:
+        print(e)
+
+
+add_sheets(Credentials.outputListSheet_ID, output_recipe_sheet)
+
+input_recipe = Credentials.sheet.values().get(spreadsheetId=Credentials.inputListSheet_ID,
+                                              range=rangeInputS).execute()
 values_input_recipe = input_recipe.get('values', [])
 
-ingredient_list = sheet.values().get(spreadsheetId=ingredientSheet_ID,
-                                     range="sheet1!A1:W6").execute()
+ingredient_list = Credentials.sheet.values().get(spreadsheetId=Credentials.ingredientSheet_ID,
+                                                 range="sheet1!A1:W6").execute()
 values_ingredient_list = ingredient_list.get('values', [])
 
 # Step 3: Write the header for columns of output file
 
 headerList = [["Name", "Zone Blocks", "Needed", "calories", "carbohydrates", "fats", "protein", "sodium", "sugar"]]
-
 
 # Step 4: Append the inputted units, name of food and zone blocks into array
 
@@ -66,17 +88,12 @@ for i, x in enumerate(values_ingredient_list):
     if inputted_names[counter] in x:
         index_inputted_food.append(i)
         counter += 1
+        if counter == len(inputted_names): # Break if all the inputted food checked
+            break
 
 # Step 6: Create variables for storing the results
 
 # Store the column of each property inside the ingredient list
-calories_column = 9
-carbs_column = 10
-fats_column = 11
-protein_column = 12
-sodium_column = 13
-sugar_column = 14
-
 gram_column = 2
 ml_column = 3
 tablespoon_column = 4
@@ -84,6 +101,22 @@ teaspoon_column = 5
 cups_column = 6
 oz_column = 7
 pieces_column = 8
+
+calories_column = 9
+carbs_column = 10
+fats_column = 11
+protein_column = 12
+sodium_column = 13
+sugar_column = 14
+
+vegan_column = 15
+vegetarian_column = 16
+glutenF_column = 17
+lacosteF_column = 18
+paleo_column = 19
+whole30_column = 20
+zone_column = 21
+zoneFav_column = 22
 
 # Final results array
 total_results_output = []
@@ -132,7 +165,7 @@ for i in range(len(values_input_recipe) - 1):
 
     # Calculate for each property
     if inputted_units[i] == "tbsp" or inputted_units[i] == "tsp":
-        needed = str(Fraction(float(zoneblocks) * float(units_value)).limit_denominator(10)) + " tbsp" # To convert
+        needed = str(Fraction(float(zoneblocks) * float(units_value)).limit_denominator(10)) + " tbsp"  # To convert
         # into fraction for table spoon and teaspoon
     elif inputted_units[i] == "tsp":
         needed = str(Fraction(float(zoneblocks) * float(units_value)).limit_denominator(10)) + " tsp"
@@ -154,7 +187,8 @@ for i in range(len(values_input_recipe) - 1):
 
     # Append to array each food for printing out result of each food
     eachFoodResult.extend(
-        [[inputted_names[i], inputted_zoneblocks[i], needed, calories, carbs, fats, protein, sodium, sugar]])
+        [[inputted_names[i], zoneblocks, needed, calories, carbs, fats, protein, sodium, sugar]])
+
     total_results_output.extend(eachFoodResult)
 
     # Reset values of each food's properties to calculate the next inputted food
@@ -168,12 +202,50 @@ for i in range(len(values_input_recipe) - 1):
 
     # Append total result for each properties
 
+#
+vegan = "yes"
+vegetarian = "yes"
+gluten_free = "yes"
+lacoste_free = "yes"
+paleo = "yes"
+whole_30 = "yes"
+zone = "yes"
+zone_Unfavorable = ""
+
+if total_sugar == 0:
+    sugar_free = "yes"
+else:
+    sugar_free = "no"
+
+for i in range(len(values_input_recipe) - 1):
+    if values_ingredient_list[index_inputted_food[i]][vegan_column] == "no":
+        vegan = "no"
+    elif values_ingredient_list[index_inputted_food[i]][vegetarian_column] == "no":
+        vegetarian = "no"
+    elif values_ingredient_list[index_inputted_food[i]][glutenF_column] == "no":
+        gluten_free = "no"
+    elif values_ingredient_list[index_inputted_food[i]][lacosteF_column] == "no":
+        lacoste_free = "no"
+    elif values_ingredient_list[index_inputted_food[i]][paleo_column] == "no":
+        paleo = "no"
+    elif values_ingredient_list[index_inputted_food[i]][whole30_column] == "no":
+        whole_30 = "no"
+    elif values_ingredient_list[index_inputted_food[i]][zone_column] == "no":
+        zone = "no"
+    elif values_ingredient_list[index_inputted_food[i]][zoneFav_column] == "no":
+        zone_Unfavorable += inputted_names[i]
+
 total_results_output.extend(
     [["", "", "TOTAL", str(total_calories) + " " + "kcal", str(total_carbs) + " " + "g", str(total_fats) + " " + "g",
       str(total_protein) + " " + "g", str(total_sodium) + " " + "mg", str(total_sugar) + " " + "g"]])
 
+total_results_output.extend([["", "", "", "", "", "", "", ""]])
+total_results_output.extend(
+    [["sugar-free", "vegan", "vegetarian", "gluten-free", "lactose-free", "paleo", "whole 30", "zone",
+      "zone-unfavorable"]])
 
-request_1 = sheet.values().update(spreadsheetId=outputListSheet_ID, range="sheet1!A1",
-                                  valueInputOption="USER_ENTERED", body={"values": total_results_output}).execute()
+total_results_output.extend(
+    [[sugar_free, vegan, vegetarian, gluten_free, lacoste_free, paleo, whole_30, zone, zone_Unfavorable]])
 
-
+request_1 = Credentials.sheet.values().update(spreadsheetId=Credentials.outputListSheet_ID, range=rangeOutputS,
+                                              valueInputOption="USER_ENTERED", body={"values": total_results_output}).execute()
