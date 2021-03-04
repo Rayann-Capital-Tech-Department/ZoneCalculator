ee9106bed3791ee71da6b5566bcc3e64a7e9de94
TuanAnh0108 <57340581+TuanAnh0108@users.noreply.github.com>
2021-03-04 17:07:34 +0900

    Update ZoneCalculator.py

--- ZoneCalculator.py
+++ ZoneCalculator.py
@@ -11,7 +11,7 @@ import helperFunctions
 input_recipe_sheet = input("Enter the input name worksheet: ")
 output_recipe_sheet = input("Enter the output name worksheet: ")
 
-rangeInputS = input_recipe_sheet + "!A1:C5"
+rangeInputS = input_recipe_sheet + "!A1:C6"
 rangeOutputS = output_recipe_sheet + "!A1"
 
 # Create output sheet with input recipe name
@@ -25,7 +25,7 @@ input_recipe = Credentials.sheet.values().get(spreadsheetId=Credentials.inputLis
 values_input_recipe = input_recipe.get('values', [])
 
 ingredient_list = Credentials.sheet.values().get(spreadsheetId=Credentials.ingredientSheet_ID,
-                                                 range="sheet1!A1:W6").execute()
+                                                 range="sheet1!A1:AA6").execute()
 values_ingredient_list = ingredient_list.get('values', [])
 
 # Step 3: Write the header for columns of output file
@@ -41,166 +41,114 @@ inputted_names = [values_input_recipe[i][0] for i in range(1, len(values_input_r
 # Step 5: Find the index row of the inputted name of foods
 
 index_inputted_food = []
-
 helperFunctions.getInputIndex(inputted_names, values_ingredient_list, index_inputted_food)
 
 # Step 6: Create variables for storing the results
 
 # Store the column of each property inside the ingredient list
 
-gram_column = 2
-ml_column = 3
-tablespoon_column = 4
-teaspoon_column = 5
-cups_column = 6
-oz_column = 7
-pieces_column = 8
-
-calories_column = 9
-carbs_column = 10
-fats_column = 11
-protein_column = 12
-sodium_column = 13
-sugar_column = 14
-
-vegan_column = 15
-vegetarian_column = 16
-glutenF_column = 17
-lacosteF_column = 18
-paleo_column = 19
-whole30_column = 20
-zone_column = 21
-zoneFav_column = 22
+measure_col = helperFunctions.ingredientList(values_ingredient_list, 2).measurement_col
+nutritions_col = helperFunctions.ingredientList(values_ingredient_list, 2).nutrians_col
+dietary_restrict_col = helperFunctions.ingredientList(values_ingredient_list, 2).dietary_restriction_col
 
 # Final results array
 total_results_output = []
 total_results_output.extend(headerList)
 
-# Variables to calculate the sum of each property
+# Dictionary to calculate the sum of each property
 
-total_calories = 0
-total_carbs = 0
-total_fats = 0
-total_protein = 0
-total_sodium = 0
-total_sugar = 0
+total_nutrians_values = helperFunctions.total_nutrions_values(values_ingredient_list)
 
 # Array to store all the property
-
 
 # Step 7: Calculate
 for i in range(len(values_input_recipe) - 1):
     # Array stores calculated results for each food
-
     eachFoodResult = []
-
-    # Reset values of each food's properties to calculate the next inputted food
-    needed = 0
-    calories = 0
-    carbs = 0
-    fats = 0
-    protein = 0
-    sodium = 0
-    sugar = 0
+    eachFoodResult.extend([inputted_names[i], inputted_zoneblocks[inputted_names[i]]])
 
     # Check the inputted unit and set the column
-
-    if inputted_units[inputted_names[i]] == "grams":
-        units_column = gram_column
-    elif inputted_units[inputted_names[i]] == "mL":
-        units_column = ml_column
-    elif inputted_units[inputted_names[i]] == "tbsp":
-        units_column = tablespoon_column
-    elif inputted_units[inputted_names[i]] == "tsp":
-        units_column = teaspoon_column
-    elif inputted_units[inputted_names[i]] == "cups":
-        units_column = cups_column
-    elif inputted_units[inputted_names[i]] == "oz":
-        units_column = oz_column
-    else:
-        units_column = pieces_column
-
+    units_column = 0
+    for propertyS in measure_col:
+        if inputted_units[inputted_names[i]] == propertyS:
+            units_column += measure_col[inputted_units[inputted_names[i]]]
+            break
+    units_value = values_ingredient_list[index_inputted_food[i]][units_column]
     # Get the value of each property inside the info list
 
-    units_value = values_ingredient_list[index_inputted_food[i]][units_column]
-    calories_value = values_ingredient_list[index_inputted_food[i]][calories_column]
-    carbs_value = values_ingredient_list[index_inputted_food[i]][carbs_column]
-    fats_value = values_ingredient_list[index_inputted_food[i]][fats_column]
-    protein_value = values_ingredient_list[index_inputted_food[i]][protein_column]
-    sodium_value = values_ingredient_list[index_inputted_food[i]][sodium_column]
-    sugar_value = values_ingredient_list[index_inputted_food[i]][sugar_column]
+    nutrions_values = helperFunctions.ingredientList(values_ingredient_list, index_inputted_food[i]).nutrians_values
 
     # Calculate for each property
-    if inputted_units[inputted_names[i]] == "tbsp" or inputted_units[inputted_names[i]] == "tsp":
+    if inputted_units[inputted_names[i]] == "tablespoon":
         needed = str(Fraction(float(inputted_zoneblocks[inputted_names[i]]) * float(units_value)).
                      limit_denominator(10)) + " tbsp"  # To convert
         # into fraction for table spoon and teaspoon
-    elif inputted_units[inputted_names[i]] == "tsp":
+    elif inputted_units[inputted_names[i]] == "teaspoon":
         needed = str(Fraction(float(inputted_zoneblocks[inputted_names[i]]) * float(units_value)).
                      limit_denominator(10)) + " tsp"
     else:
         needed = str(round(float(inputted_zoneblocks[inputted_names[i]]) * float(units_value), 0)) \
                  + " " + str(inputted_units[inputted_names[i]])
 
-    propertyArray = [calories, carbs, fats, protein, sodium, sugar]
-    property_valueArray = [calories_value, carbs_value, fats_value, protein_value, sodium_value, sugar_value]
-    totalPropertyArray = [total_calories, total_carbs, total_fats, total_protein, total_sodium, total_sugar]
-
-    for j in range(len(propertyArray)):
-        propertyArray[j] = round(float(inputted_zoneblocks[inputted_names[i]]) * float(property_valueArray[j]))
-        totalPropertyArray[j] += propertyArray[j]
+    eachFoodResult.append(needed)
+    for nutrion in nutrions_values:
+        nutrions_values[nutrion] *= round(float(inputted_zoneblocks[inputted_names[i]]))
+        total_nutrians_values[nutrion] += nutrions_values[nutrion]
+        eachFoodResult.append(nutrions_values[nutrion])
 
     # Append to array each food for printing out result of each food
-    eachFoodResult.extend(
-        [[inputted_names[i], inputted_zoneblocks[inputted_names[i]], needed, calories, carbs, fats, protein, sodium,
-          sugar]])
 
-    total_results_output.extend(eachFoodResult)
+    total_results_output.extend([eachFoodResult])
 
 # Add restrict
 vegan = "yes"
 vegetarian = "yes"
 gluten_free = "yes"
-lacoste_free = "yes"
+lactose_free = "yes"
 paleo = "yes"
 whole_30 = "yes"
 zone = "yes"
 zone_Unfavorable = ""
 
-if total_sugar == 0:
+if total_nutrians_values["sugar"] == 0:
     sugar_free = "yes"
 else:
     sugar_free = "no"
 
 for i in range(len(values_input_recipe) - 1):
-    if values_ingredient_list[index_inputted_food[i]][vegan_column] == "no":
+    if values_ingredient_list[index_inputted_food[i]][dietary_restrict_col["vegan"]] == "no":
         vegan = "no"
-    elif values_ingredient_list[index_inputted_food[i]][vegetarian_column] == "no":
+    elif values_ingredient_list[index_inputted_food[i]][dietary_restrict_col["vegetarian"]] == "no":
         vegetarian = "no"
-    elif values_ingredient_list[index_inputted_food[i]][glutenF_column] == "no":
+    elif values_ingredient_list[index_inputted_food[i]][dietary_restrict_col["gluten-free"]] == "no":
         gluten_free = "no"
-    elif values_ingredient_list[index_inputted_food[i]][lacosteF_column] == "no":
-        lacoste_free = "no"
-    elif values_ingredient_list[index_inputted_food[i]][paleo_column] == "no":
+    elif values_ingredient_list[index_inputted_food[i]][dietary_restrict_col["lactose-free"]] == "no":
+        lactose_free = "no"
+    elif values_ingredient_list[index_inputted_food[i]][dietary_restrict_col["paleo"]] == "no":
         paleo = "no"
-    elif values_ingredient_list[index_inputted_food[i]][whole30_column] == "no":
+    elif values_ingredient_list[index_inputted_food[i]][dietary_restrict_col["whole 30"]] == "no":
         whole_30 = "no"
-    elif values_ingredient_list[index_inputted_food[i]][zone_column] == "no":
+    elif values_ingredient_list[index_inputted_food[i]][dietary_restrict_col["zone"]] == "no":
         zone = "no"
-    elif values_ingredient_list[index_inputted_food[i]][zoneFav_column] == "no":
+    elif values_ingredient_list[index_inputted_food[i]][dietary_restrict_col["zone-favorable"]] == "no":
         zone_Unfavorable += inputted_names[i]
 
-total_results_output.extend(
-    [["", "", "TOTAL", str(total_calories) + " " + "kcal", str(total_carbs) + " " + "g", str(total_fats) + " " + "g",
-      str(total_protein) + " " + "g", str(total_sodium) + " " + "mg", str(total_sugar) + " " + "g"]])
 
-total_results_output.extend([["", "", "", "", "", "", "", ""]])
-total_results_output.extend(
-    [["sugar-free", "vegan", "vegetarian", "gluten-free", "lactose-free", "paleo", "whole 30", "zone",
-      "zone-unfavorable"]])
+totalNutriansArray = ["", "", "TOTAL"]
+for nutrition in total_nutrians_values:
+    totalNutriansArray.append(total_nutrians_values[nutrition])
+
+total_results_output.extend([totalNutriansArray])
+total_results_output.extend([[""]])
+
+restrictionArray = []
+for restriction in dietary_restrict_col:
+    restrictionArray.append(restriction)
+
+total_results_output.extend([restrictionArray])
 
 total_results_output.extend(
-    [[sugar_free, vegan, vegetarian, gluten_free, lacoste_free, paleo, whole_30, zone, zone_Unfavorable]])
+    [[sugar_free, vegan, vegetarian, gluten_free, lactose_free, paleo, whole_30, zone, zone_Unfavorable]])
 
 request_1 = Credentials.sheet.values().update(spreadsheetId=Credentials.outputListSheet_ID, range=rangeOutputS,
                                               valueInputOption="USER_ENTERED",
