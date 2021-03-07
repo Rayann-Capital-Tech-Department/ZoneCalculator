e1c1e32be36df94286c5bf645112cfa293c7ecc6
TuanAnh0108 <57340581+TuanAnh0108@users.noreply.github.com>
2021-03-07 14:32:25 +0900

    Update Credentials.py

--- Credentials.py
+++ Credentials.py
@@ -11,9 +11,9 @@ creds = service_account.Credentials.from_service_account_file(
 # If modifying these scopes, delete the file token.pickle.
 
 # Step 1: The ID of the spreadsheets
-ingredientSheet_ID = '1VkSVE1wUSWzIL6tL7iz9D4fsSvkgSY7svvwkmLb9_e0'
-inputListSheet_ID = '1ecMsdBj8GK1HL2ps-ouV0Zu3fqLD-BYmCylM5KZb_MA'
-outputListSheet_ID = '1fV96CIiY57L4Xje0Kr7BLIwt0Gjoyuq1B4-mpIamkVk'
+ingredientSheet_ID = '1FihBuWgOUalxuA_b77RPCXoSEgr3Crcrt1MHAH4ts1I'
+inputListSheet_ID = '1PAx65UAcSWvHb9_cs2h4ooGFjgfKY_mhjGAcB8Ic_4E'
+outputListSheet_ID = '1g10_SjiUuvz9am2ADL7yzOZfREGAtP-Jy8NumTt4RzI'
 
 service = build('sheets', 'v4', credentials=creds)
 
